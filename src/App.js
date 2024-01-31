9f6fb5e0e34ec9b366219faf82e28e86311943c1
RiyaadHossain <riyadhossain.dev@gmail.com>
2024-01-31 16:07:00 +0600

    toast fixed

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Toaster } from "react-hot-toast";
 import "./styles/utility.style.css";
 import "./styles/details-page.style.css";
 import "./styles/typography.style.css";
+import { RouterProvider } from "react-router-dom";
+import routes from "./routes/routes";
 
 function App() {
-
   return (
     <div>
       <Toaster />
+      <RouterProvider router={routes} />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,17 +4,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
-import { RouterProvider } from "react-router-dom";
-import routes from "./routes/routes";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <RouterProvider router={routes}>
         <App />
-      </RouterProvider>
     </Provider>
   </React.StrictMode>
 );
